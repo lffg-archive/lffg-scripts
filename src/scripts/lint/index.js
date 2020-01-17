2ae8465c97f86a534bd90a119f9f0cb2277fa109
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2020-01-16 23:39:38 -0300

    feat: Add `lint` script

--- src/scripts/lint/index.js
+++ src/scripts/lint/index.js
@@ -1,5 +1,39 @@
+const debug = require('debug')('lffg-scripts:lint');
+const { forwardArg } = require('../../utils/argv');
+const {
+  hasFile,
+  hasPkgProp,
+  exec,
+  resolveConfigPath
+} = require('../../utils/script-helpers');
+
+/**
+ *
+ */
 function run() {
-  console.log('--> FROM LINT!');
+  const useBuiltInConfig =
+    !hasFile('.eslintrc') &&
+    !hasFile('.eslintrc.js') &&
+    !hasFile('.eslintrc.json') &&
+    !hasPkgProp('eslintConfig');
+
+  debug(`Built in config will be used? ${useBuiltInConfig}`);
+  const config = useBuiltInConfig
+    ? ['--config', resolveConfigPath('.eslintrc.json')]
+    : [];
+
+  const args = [
+    forwardArg('--fix'),
+    ...config,
+    '--max-warnings',
+    '0',
+    '.'
+  ].filter(Boolean);
+
+  debug('Args: %o', args);
+
+  const { status } = exec('eslint', args);
+  process.exit(status);
 }
 
 module.exports = {
